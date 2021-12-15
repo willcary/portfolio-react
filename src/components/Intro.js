3cd2988dc5a4601cb77d4d365cdbdfa72cfd3002
Will Cary <w.a.c.ary@gmail.com>
2021-12-15 09:53:47 -0500

    Updated Hero Section with Light Background Image.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,11 +3,13 @@ import introImg from '../images/LakeG.jpg'
 
 function Intro() {
     return (
-        <section className="intro" id="home">
-            <h1 className="section__title section__title--intro">Hi, I'm <strong>Will Cary</strong></h1> 
-            <p className="section__subtitle section__subtitle--intro">front-end developer</p>
-            <img src={introImg} alt="Will Cary portrait with his dog" className="intro__img" />
-        </section>
+        <div className="light-background">
+            <section className="intro" id="home">
+                <h1 className="section__title section__title--intro">Hi, I'm <strong>Will Cary</strong></h1> 
+                <p className="section__subtitle section__subtitle--intro">front-end developer</p>
+                <img src={introImg} alt="Will Cary portrait with his dog" className="intro__img" />
+            </section>
+        </div>
     )
 }
 
