1909f27731bf44858274e94eb6be4292adfeb1be
Will Cary <w.a.c.ary@gmail.com>
2022-01-10 13:55:14 -0500

    Update About Section and Projects Images.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import aboutImg from '../images/Will-on-boulder.jpg'
 import './../css/about.css'
 
 function About() {
@@ -11,18 +10,17 @@ function About() {
                 <p>I am a motivated developer with experience designing and implementing responsive websites and apps. I enjoy making things that are aesthetically pleasing, functional, and useful. I'm a perpetual learner. Along the way, I have earned both a B.A and M.S. in Geology, I've taught myself web development, and even taught both science and coding to 4-6th grade students for 3 years. My broad background has prepared me well to learn and think logically. However, I also thrive when prompted to produce original, inventive ideas. I find coding to be the perfect balance of logic and creativity. I'm constantly working to improve and broaden my skillset with relevant technologies.</p>
                 <p>When I'm not coding, I'm usually pursuing some outdoor activity inluding trail running, hiking, mountain biking, cross country skiing, paddleboarding, etc...</p>
             </div>
-            <img src={aboutImg} alt="Will and his dog on a boulder" className="about-me__img" />
             <div className="about-me__skills">
-                <h3>Skills</h3>
+                <h3 className="about-me__skills-title">Skills</h3>
                 <div className="about-me__skills-box">
                     <div className="about-me__skills-item">React</div>
                     <div className="about-me__skills-item">HTML</div>
                     <div className="about-me__skills-item">CSS</div>
                     <div className="about-me__skills-item">JavaScript</div>
                     <div className="about-me__skills-item">Responsive Design</div>
+                    <div className="about-me__skills-item">Git</div>
                     <div className="about-me__skills-item">Bootstrap</div>
                     <div className="about-me__skills-item">Scss</div>
-                    <div className="about-me__skills-item">Git</div>
                 </div>
             </div>
         </section>
